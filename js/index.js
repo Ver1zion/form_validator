89c875c39e2fad54d64576a26a2680712f1fede2
Igor <smmdirektolog@yandex.ru>
2024-04-21 14:21:14 +0300

    +

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
+const cartButton = document.querySelector(".cart-button");
 const loginButton = document.querySelector(".login-button");
 const signUpButton = document.querySelector(".sign-up-button");
+const cartForm = document.querySelector(".cart-form");
 const loginForm = document.querySelector(".login-form");
 const signUpForm = document.querySelector(".sign-up-form");
+const menuWrapp = document.querySelector(".menu_wrapp");
+const orderButtonWrapp = document.querySelector(".order-Button_Wrapp");
+let quantity = {
+  burger: 0,
+  piza: 0,
+};
+let total = {
+  burger: 0,
+  piza: 0,
+};
+
+cartButton.addEventListener("click", () => {
+  if (cartForm.style.transform === "translate(-50%, -50%) scale(0)") {
+    cartForm.style.transform = "translate(-50%, -50%) scale(1)";
+    cartForm.style.zIndex = "10";
+    signUpForm.style.transform = "translate(-50%, -50%) scale(0)";
+    signUpForm.style.zIndex = "1";
+    loginForm.style.transform = "translate(-50%, -50%) scale(0)";
+    loginForm.style.zIndex = "1";
+  } else {
+    cartForm.style.transform = "translate(-50%, -50%) scale(0)";
+  }
+});
 
 loginButton.addEventListener("click", () => {
   if (loginForm.style.transform === "translate(-50%, -50%) scale(0)") {
@@ -9,6 +34,8 @@ loginButton.addEventListener("click", () => {
     loginForm.style.zIndex = "10";
     signUpForm.style.transform = "translate(-50%, -50%) scale(0)";
     signUpForm.style.zIndex = "1";
+    cartForm.style.transform = "translate(-50%, -50%) scale(0)";
+    cartForm.style.zIndex = "1";
   } else {
     loginForm.style.transform = "translate(-50%, -50%) scale(0)";
   }
@@ -20,7 +47,80 @@ signUpButton.addEventListener("click", () => {
     signUpForm.style.zIndex = "10";
     loginForm.style.transform = "translate(-50%, -50%) scale(0)";
     loginForm.style.zIndex = "1";
+    cartForm.style.transform = "translate(-50%, -50%) scale(0)";
+    cartForm.style.zIndex = "1";
   } else {
     signUpForm.style.transform = "translate(-50%, -50%) scale(0)";
+  }
+});
+
+menuWrapp.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("card-button") &&
+    event.target.closest(".burger")
+  ) {
+    quantity.burger += 1;
+    total.burger += 500;
+    document.dataLayer.push({
+      ecommerce: {
+        currencyCode: "RUB",
+        add: {
+          products: [
+            {
+              id: "1",
+              name: "Бургер",
+              price: 500,
+              brand: "Яндекс",
+              category: "Еда",
+              quantity: 1,
+            },
+          ],
+        },
+      },
+    });
+    console.log(quantity.burger);
+    let quantityBurgers = cartForm.querySelector(".quantity_burgers");
+    let totalSum = cartForm.querySelector(".total");
+
+    if (!quantityBurgers) {
+      quantityBurgers = document.createElement("div");
+      quantityBurgers.classList.add("quantity_burgers");
+      cartForm.prepend(quantityBurgers);
+    }
+
+    if (!totalSum) {
+      totalSum = document.createElement("div");
+      totalSum.classList.add("total");
+      orderButtonWrapp.prepend(totalSum);
+    }
+
+    totalSum.innerText = `Rub: ${total.piza + total.burger}`;
+    quantityBurgers.innerText = `Burger: ${quantity.burger}`;
+  }
+
+  if (
+    event.target.classList.contains("card-button") &&
+    event.target.closest(".piza")
+  ) {
+    quantity.piza += 1;
+    total.piza += 1000;
+    console.log(quantity.piza);
+    let quantityPizas = cartForm.querySelector(".quantity_pizas");
+    let totalSum = cartForm.querySelector(".total");
+
+    if (!quantityPizas) {
+      quantityPizas = document.createElement("div");
+      quantityPizas.classList.add("quantity_pizas");
+      cartForm.prepend(quantityPizas);
+    }
+
+    if (!totalSum) {
+      totalSum = document.createElement("div");
+      totalSum.classList.add("total");
+      orderButtonWrapp.prepend(totalSum);
+    }
+
+    totalSum.innerText = `Rub: ${total.piza + total.burger}`;
+    quantityPizas.innerText = `Piza: ${quantity.piza}`;
   }
 });
