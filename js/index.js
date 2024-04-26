a0d477c0b597a1dead3248940d1211e688c4a7e1
Igor <smmdirektolog@yandex.ru>
2024-04-26 14:06:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -209,6 +209,6 @@ menuWrapp.addEventListener("click", (event) => {
   }
 });
 
-// ym(97087644, "userParams", {
-//   status: "unauthorized",
-// });
+ym(97087644, "userParams", {
+  status: "unauthorized",
+});
